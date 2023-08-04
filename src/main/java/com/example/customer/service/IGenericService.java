80e77e0be9d4737d1199742ee99240289805aa42
Tranhuy1612 <huytran1612@icloud.com>
2023-08-04 09:00:18 +0700

    Md4-ss12

---
+++ src/main/java/com/example/customer/service/IGenericService.java
@@ -0,0 +1,10 @@
+package com.example.customer.service;
+
+import java.util.List;
+
+public interface IGenericService<T,E> {
+    List<T> findAll();
+    void save(T t);
+    T findById(E e);
+    void deleteById(E e);
+}
